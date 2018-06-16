8e87b44968665e7bc713f21cfd4275fdc3a07041
thai2net <thai2net@hotmail.com>
2018-06-16 13:08:40 +0700

    update 16-06-2018 10:59 AM

--- Chapter06/src/ch06_04.php
+++ Chapter06/src/ch06_04.php
@@ -1,11 +1,11 @@
 <?php // ตัวอย่างโปรแกรมที่ 6.4 การสร้างคุกกี้ขึ้นมาใช้งาน
    ob_start();
-   $txtName = "Surachart Boachum";
+   $txtName = "Steve Rogers";
    $txtType = "Administrator";
    setcookie("UserName", $txtName, time()+3600);
    setcookie("UserType", $txtType, time()+3600);
    echo "<B>Set Cookies</B><BR>"; 
-   echo "UserName: ".$_COOKIE['UserName']."<BR>";
-   echo "UserType: ".$_COOKIE['UserType'];
+   echo "UserName: ".$txtName."<BR>";
+   echo "UserType: ".$txtType;
    ob_end_flush();
 ?>
