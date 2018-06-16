1aac397073c2f8d1775d84942c781ca2591156ba
thai2net <thai2net@hotmail.com>
2018-06-16 14:00:05 +0700

    update 16-06-2018 10:59 AM

--- Chapter06/src/ch06_10.php
+++ Chapter06/src/ch06_10.php
@@ -1,6 +1,6 @@
 <?php // ตัวอย่างโปรแกรมที่ 6.10 การเขียนไฟล์ตามข้อความที่กำหนด
-   $myfile = fopen("userlog.txt", "a+") or die("Unable to open file!");
-   $username = "Surachart"; 
+   $myfile = fopen("lastlog.txt", "w+") or die("Unable to open file!");
+   $username = "Steve Rogers"; 
    $action = "DELETE";
    $date = date_format(date_create(), 'Y-m-d H:i:s');
    $txt = $username.", ".$action.", ".$date."\n";
--- Chapter06/src/ch06_11.php
+++ Chapter06/src/ch06_11.php
@@ -1,6 +1,6 @@
 <?php // ตัวอย่างโปรแกรมที่ 6.11 การเขียนไฟล์ทีละบรรทัด
-   $myfile = fopen("last_userlog.txt", "w") or die("Unable to open file!");
-   $username = "Surachart"; 
+   $myfile = fopen("adminlog.txt", "w+") or die("Unable to open file!");
+   $username = "Tony Stark"; 
    $action = "DELETE";
    $date = date_format(date_create(), 'Y-m-d H:i:s');
    $txt = $username.", ".$action.", ".$date."\n";
