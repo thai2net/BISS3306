e165ad62b4fa28bf02fadc6c4160cfcea94e29bb
thai2net <thai2net@hotmail.com>
2018-06-16 12:51:55 +0700

    update 16-06-2018 10:59 AM

--- Chapter06/src/ch06_01.php
+++ Chapter06/src/ch06_01.php
@@ -1,6 +1,6 @@
 <?php // ตัวอย่างโปรแกรมที่ 6.1 การเริ่มต้นเซสชันและการกำหนดค่าเซสชัน
    session_start();
-   $_SESSION['UserName'] = "Surachart Boachum";
+   $_SESSION['UserName'] = "Steve Rogers";
    $_SESSION["UserType"] = "Administrator";
    session_write_close();
    echo "<B>Started and Registered Session</B><BR>";
